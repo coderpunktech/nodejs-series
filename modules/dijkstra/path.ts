81ecc55fd83f007db9d2bc9ef4d7a55378ba4312
Mass <mexassi85@gmail.com>
2022-03-02 21:11:04 +1100

    fixing compile issue on Path class

--- modules/dijkstra/path.ts
+++ modules/dijkstra/path.ts
@@ -1,5 +1,10 @@
+import List from "../common/collections/list";
 import Edge from "./edge";
 
 export default class Path {
     edges: List<Edge>
+
+    constructor(edges: List<Edge>) {
+        this.edges = edges;
+    }
 }
